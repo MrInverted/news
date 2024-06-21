1ca1641a21030490d0508f6644ca993569ccdade
MrInverted <akson.dyuk@gmail.com>
2024-06-21 11:30:43 +0300

    some fixes

--- js/main.js
+++ js/main.js
@@ -12,18 +12,20 @@ class Popup {
   }
 }
 
-const suggestNewsTriggers = document.querySelectorAll("[suggest-news]");
-const suggestNewsCloseTrigger = document.querySelector("#suggest-news [close]");
-const suggestNewsPopup = new Popup(document.getElementById("suggest-news"));
-
-suggestNewsTriggers.forEach(item => item.onclick = () => suggestNewsPopup.open());
-suggestNewsCloseTrigger.onclick = () => suggestNewsPopup.close();
-
-
-
 const headerDrawerTriggers = document.querySelectorAll("[mobile-drawer]");
 const headerDrawerCloseTrigger = document.querySelector("#header-drawer [close]");
 const headerDrawerPopup = new Popup(document.getElementById("header-drawer"));
 
 headerDrawerTriggers.forEach(item => item.onclick = () => headerDrawerPopup.open())
 headerDrawerCloseTrigger.onclick = () => headerDrawerPopup.close();
+
+
+const suggestNewsTriggers = document.querySelectorAll("[suggest-news]");
+const suggestNewsCloseTrigger = document.querySelector("#suggest-news [close]");
+const suggestNewsPopup = new Popup(document.getElementById("suggest-news"));
+
+suggestNewsTriggers.forEach(item => item.onclick = () => {
+  suggestNewsPopup.open();
+  headerDrawerPopup.close();
+});
+suggestNewsCloseTrigger.onclick = () => suggestNewsPopup.close();
